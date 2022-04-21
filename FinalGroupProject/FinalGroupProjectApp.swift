f856431adc793cad8878058a29608a3420cef64d
oleksii-pedko <61472535+oleksii-pedko@users.noreply.github.com>
2022-04-20 21:08:29 -0400

    Initial Commit

---
+++ FinalGroupProject/FinalGroupProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalGroupProjectApp.swift
+//  FinalGroupProject
+//
+//  Created by Alex Pedko on 20.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct FinalGroupProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
